77e606f016bb27e4d30e6a5540637210a06fb15c
Kisore <lakisore111@gmail.com>
2021-12-13 21:03:54 +0530

    add error middleware

--- src/server.js
+++ src/server.js
@@ -4,17 +4,26 @@ import { Server } from "socket.io";
 import cors from "cors";
 import dotenv from "dotenv";
 import socketHandler from "./socket.js";
+import { notFound, errorHandler } from "./middlewares/errorMiddleware.js";
 
 dotenv.config();
 
 const app = express();
+app.use(express.json());
+app.use(cors());
+
 const httpServer = createServer(app);
-const io = new Server(httpServer);
+const io = new Server(httpServer, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+});
 const PORT = process.env.PORT || 3001;
 socketHandler(io);
 
-app.use(express.json());
-app.use(cors());
+app.use(notFound);
+app.use(errorHandler);
 
 httpServer.listen(PORT, () => {
   console.log(`server is running on ${PORT}`);
